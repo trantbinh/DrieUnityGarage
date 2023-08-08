737f3cacebb2c5ed7fa22a1f67413466ec7f976b
b3tran <tranthybinh@gmail.com>
2023-08-09 00:00:34 +0700

    Tài khoản nhân vien done

--- DrieUnityGarage/DrieUnityGarage/Controllers/NHANVIENController.cs
+++ DrieUnityGarage/DrieUnityGarage/Controllers/NHANVIENController.cs
@@ -79,31 +79,235 @@ namespace DrieUnityGarage.Controllers
         //-----------------DANH SÁCH TÀI KHOẢN---------------//
         public ActionResult LayDanhSachTaiKhoan()
         {
-            var tk = db.NHANVIENs.ToList();
+            var tk = db.NHANVIENs.Where(m => m.TenDangNhap != null).ToList();
             return View(tk);
         }
-
-
         public ActionResult TaoTaiKhoan()
         {
-            ViewBag.MaNV = new SelectList(db.NHANVIENs, "MaNV", "MaNV");
+            var nvDB = db.NHANVIENs.ToList();
+            var newlstNV = new List<THONGTINNHANVIEN>();
+            for (int i =0; i< nvDB.Count(); i++)
+            {
+                if (nvDB[i].TenDangNhap != null || nvDB[i].MatKhau!=null)
+                    continue;
+                else
+                    newlstNV.Add(new THONGTINNHANVIEN(nvDB[i].MaNV));
+            }
+            ViewBag.lstNhanVien = new SelectList(newlstNV, "MaNV", "ThongTin");
+
+            String date = DateTime.Now.ToString("dd/MM/yyyy");
+            ViewBag.NgayTaoTK = date;
+
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult TaoTaiKhoan([Bind(Include = "TenDangNhap,MatKhau,NgayTaoTK")]  NHANVIEN nv)
+        public ActionResult TaoTaiKhoan([Bind(Include = "MaNV,HoTenNV,DienThoaiNV,NgaySinh,GioiTinh,Email,DiaChi,ChucVu,PhongBan,TenDangNhap,MatKhau,NgayTaoTK")] NHANVIEN nv, String lstNhanVien, String XacNhanMK)
         {
             if (ModelState.IsValid)
             {
-               
-                db.Entry(nv).State = EntityState.Modified;
-                db.SaveChanges(); 
-                return RedirectToAction("LayDanhSachTaiKhoan");
+                if(nv.MatKhau != XacNhanMK)
+                    ModelState.AddModelError(string.Empty, "Mật khẩu không khớp!");
+
+                if (ModelState.IsValid)
+                {
+                    nv.MaNV = lstNhanVien;
+                    nv.HoTenNV = "";
+                    nv.ChucVu = "";
+                    nv.DienThoaiNV = "";
+                    nv.NgaySinh = DateTime.Now;
+                    nv.GioiTinh = "";
+                    nv.Email = "";
+                    nv.DiaChi = "";
+                    nv.PhongBan = "";
+
+                    nv.NgayTaoTK = DateTime.Now;
+                    db.Entry(nv).State = EntityState.Modified;
+                    db.Entry(nv).Property(s => s.MatKhau).IsModified = true;
+                    db.Entry(nv).Property(s => s.TenDangNhap).IsModified = true;
+                    db.Entry(nv).Property(s => s.NgayTaoTK).IsModified = true;
+                    db.Entry(nv).Property(s => s.HoTenNV).IsModified = false;
+                    db.Entry(nv).Property(s => s.ChucVu).IsModified = false;
+                    db.Entry(nv).Property(s => s.DienThoaiNV).IsModified = false;
+                    db.Entry(nv).Property(s => s.NgaySinh).IsModified = false;
+                    db.Entry(nv).Property(s => s.GioiTinh).IsModified = false;
+                    db.Entry(nv).Property(s => s.Email).IsModified = false;
+                    db.Entry(nv).Property(s => s.DiaChi).IsModified = false;
+                    db.Entry(nv).Property(s => s.PhongBan).IsModified = false;
+                    db.SaveChanges();
+                    return RedirectToAction("LayDanhSachTaiKhoan");
+                }
+                ViewBag.MaNV = new SelectList(db.NHANVIENs, "MaNV", "MaNV");
+                var nvDB = db.NHANVIENs.ToList();
+                var newlstNV = new List<THONGTINNHANVIEN>();
+                for (int i = 0; i < nvDB.Count(); i++)
+                {
+                    if (nvDB[i].TenDangNhap != null || nvDB[i].MatKhau != null)
+                        continue;
+                    else
+                        newlstNV.Add(new THONGTINNHANVIEN(nvDB[i].MaNV));
+                }
+                ViewBag.lstNhanVien = new SelectList(newlstNV, "MaNV", "ThongTin",nv.MaNV);
+
+
             }
-            ViewBag.MaNV = new SelectList(db.NHANVIENs, "MaNV", "MaNV");
             return View(nv);
         }
+
+        // GET: NHANVIEN/Edit/5
+        public ActionResult SuaTaiKhoan(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NHANVIEN nHANVIEN = db.NHANVIENs.Find(id);
+            if (nHANVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            String nv = id + " - " + nHANVIEN.HoTenNV + " - "+nHANVIEN.ChucVu;
+            ViewBag.selectedNhanVien = nv;
+
+            DateTime date = (DateTime)nHANVIEN.NgayTaoTK;
+            String dateFormat = date.ToString("dd/MM/yyyy");
+            ViewBag.NgayTaoTK = dateFormat;
+            Session["MaNV"] = id;
+            return View(nHANVIEN);
+        }
+
+        // POST: NHANVIEN/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SuaTaiKhoan([Bind(Include = "MaNV,HoTenNV,DienThoaiNV,NgaySinh,GioiTinh,Email,DiaChi,ChucVu,PhongBan,TenDangNhap,MatKhau,NgayTaoTK")] NHANVIEN nv, String XacNhanMK)
+        {
+            var tendn = db.NHANVIENs.FirstOrDefault(k => k.TenDangNhap.Equals(nv.TenDangNhap));
+            if (tendn != null)
+                ModelState.AddModelError(string.Empty, "Tên đăng nhập đã tồn tại!");
+            if (nv.MatKhau != XacNhanMK)
+                ModelState.AddModelError(string.Empty, "Mật khẩu không khớp!");
+
+            if (ModelState.IsValid)
+            {
+                nv.MaNV = Session["MaNV"].ToString();
+                nv.HoTenNV = "";
+                nv.ChucVu = "";
+                nv.DienThoaiNV = "";
+                nv.NgaySinh = DateTime.Now;
+                nv.GioiTinh = "";
+                nv.Email = "";
+                nv.DiaChi = "";
+                nv.PhongBan = "";
+
+                nv.NgayTaoTK = DateTime.Now;
+                db.Entry(nv).State = EntityState.Modified;
+                db.Entry(nv).Property(s => s.MatKhau).IsModified = true;
+                db.Entry(nv).Property(s => s.TenDangNhap).IsModified = true;
+                db.Entry(nv).Property(s => s.NgayTaoTK).IsModified = true;
+                db.Entry(nv).Property(s => s.HoTenNV).IsModified = false;
+                db.Entry(nv).Property(s => s.ChucVu).IsModified = false;
+                db.Entry(nv).Property(s => s.DienThoaiNV).IsModified = false;
+                db.Entry(nv).Property(s => s.NgaySinh).IsModified = false;
+                db.Entry(nv).Property(s => s.GioiTinh).IsModified = false;
+                db.Entry(nv).Property(s => s.Email).IsModified = false;
+                db.Entry(nv).Property(s => s.DiaChi).IsModified = false;
+                db.Entry(nv).Property(s => s.PhongBan).IsModified = false;
+
+                db.SaveChanges();
+                return RedirectToAction("LayDanhSachTaiKhoan");
+            }
+            return View(nv);
+        }
+        // GET: NHANVIEN/Details/5
+        public ActionResult LayThongTinTaiKhoan(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NHANVIEN nHANVIEN = db.NHANVIENs.Find(id);
+            if (nHANVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            String nv = id + " - " + nHANVIEN.HoTenNV + " - " + nHANVIEN.ChucVu;
+            ViewBag.selectedNhanVien = nv;
+
+            DateTime date = (DateTime)nHANVIEN.NgayTaoTK;
+            String dateFormat = date.ToString("dd/MM/yyyy");
+            ViewBag.NgayTaoTK = dateFormat;
+
+            return View(nHANVIEN);
+        }
+        public ActionResult XoaTaiKhoan(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NHANVIEN nHANVIEN = db.NHANVIENs.Find(id); ;
+
+                if (nHANVIEN == null)
+                {
+                    return HttpNotFound();
+                }
+
+            String nv = id + " - " + nHANVIEN.HoTenNV + " - " + nHANVIEN.ChucVu;
+            ViewBag.selectedNhanVien = nv;
+
+            DateTime date = (DateTime)nHANVIEN.NgayTaoTK;
+            String dateFormat = date.ToString("dd/MM/yyyy");
+            ViewBag.NgayTaoTK = dateFormat;
+
+            return View(nHANVIEN);
+        }
+
+        // POST: NHANVIEN/Delete/5
+        [HttpPost, ActionName("XoaTaiKhoan")]
+        [ValidateAntiForgeryToken]
+        public ActionResult XoaTaiKhoanConfirmed(string id)
+        {
+            NHANVIEN nv = db.NHANVIENs.Find(id);
+            nv.MaNV = id;
+            nv.HoTenNV = "";
+            nv.ChucVu = "";
+            nv.DienThoaiNV = "";
+            nv.NgaySinh = DateTime.Now;
+            nv.GioiTinh = "";
+            nv.Email = "";
+            nv.DiaChi = "";
+            nv.PhongBan = "";
+            nv.TenDangNhap = null;
+            nv.MatKhau = null;
+            nv.NgayTaoTK = null;
+
+            db.Entry(nv).State = EntityState.Modified;
+            db.Entry(nv).Property(s => s.MatKhau).IsModified = true;
+            db.Entry(nv).Property(s => s.TenDangNhap).IsModified = true;
+            db.Entry(nv).Property(s => s.NgayTaoTK).IsModified = true;
+            db.Entry(nv).Property(s => s.HoTenNV).IsModified = false;
+            db.Entry(nv).Property(s => s.ChucVu).IsModified = false;
+            db.Entry(nv).Property(s => s.DienThoaiNV).IsModified = false;
+            db.Entry(nv).Property(s => s.NgaySinh).IsModified = false;
+            db.Entry(nv).Property(s => s.GioiTinh).IsModified = false;
+            db.Entry(nv).Property(s => s.Email).IsModified = false;
+            db.Entry(nv).Property(s => s.DiaChi).IsModified = false;
+            db.Entry(nv).Property(s => s.PhongBan).IsModified = false;
+
+            db.SaveChanges();
+            return RedirectToAction("LayDanhSachTaiKhoan");
+        }
+
+
+
+
+
+
+
+
 
 
         // GET: NHANVIEN
@@ -250,7 +454,7 @@ namespace DrieUnityGarage.Controllers
         public bool KiemTraKhoaNgoaiNhanVien(string id)
         {
             List<THONGTINTIEPNHAN> tn = db.THONGTINTIEPNHANs.Where(m => m.TN_MaNV.Equals(id)).ToList();
-            if (tn != null)
+            if (tn.Count()!=0)
             {
                 return true;
             }
